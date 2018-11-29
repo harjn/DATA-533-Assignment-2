0ccce2aab55a2f66f4418e9854ed0d635283cfe3
harjn <harjot_550@msn.com>
2018-11-29 15:44:00 -0800

    3rd commit

--- People/Customer.py
+++
@@ -1,8 +0,0 @@
-class Customer:
-    emails=[]
-    def __init__(self, customerID, email):
-        self.customerID = customerID
-        Customer.emails.append(email)
-        
-    def allEmails(self):
-        return Customer.emails
